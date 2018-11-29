42a15ab5d6121aac4de71811f50bde27197d1e9c
Elton Zhong <elton.zhong@LMXMN150.local>
2018-11-29 21:43:19 +0800

    Make Validator method static and add readme

--- src/main/java/com/ez/tools/validator/core/Validator.java
+++ src/main/java/com/ez/tools/validator/core/Validator.java
@@ -6,7 +6,7 @@ import com.ez.tools.validator.core.detail.MethodValidator;
 import java.util.Arrays;
 
 public class Validator {
-    public void validate(Object o) {
+    public static void validate(Object o) {
         Arrays.stream(o.getClass().getDeclaredFields()).forEach(field -> new FieldValidator(field, o).validate());
         Arrays.stream(o.getClass().getDeclaredMethods()).forEach(method -> new MethodValidator(method, o).validate());
     }
